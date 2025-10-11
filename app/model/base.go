4de2b58ad74d575c0b469466f384ca5985a2b31b
guoliang <bit-labs@qq.com>
2025-10-11 10:03:53 +0800

    chore: 基础数据库字段，增加 json 返回

--- app/model/base.go
+++ app/model/base.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Base struct {
-	ID        uint           `gorm:"<-:create" gorm:"primarykey" json:"id,string"`
-	CreatedAt time.Time      `gorm:"<-:create" json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID        uint           `gorm:"<-:create" gorm:"primarykey" json:"id,string,omitempty"`
+	CreatedAt time.Time      `gorm:"<-:create" json:"createdAt,omitempty"`
+	UpdatedAt time.Time      `json:"updatedAt" json:"updatedAt,omitempty"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
 }
